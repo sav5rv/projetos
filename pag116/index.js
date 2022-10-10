090d5c656d8de3c38aa30f575b7160501d14057d
sav5rv <wsavioli@gmail.com>
2022-10-10 10:31:37 -0300

    testando cod pag116

--- pag116/index.js
+++ pag116/index.js
@@ -1,5 +1,6 @@
 const express = require('express')   
 const fs      = require('fs')   //fs é o módulo file-system, para ler arquivos 
+
 const app     = express() 
 
 app.get('/',processRequest)
@@ -9,10 +10,10 @@ function processRequest(request, response) {
     console.log('requisição terminou')
 }
 
-function readText(request, response){
-    //salve um arquivo teste.txt junto a esse arquivo com qualquer coisa dentro 
+function readText(request, response) {
+    //crie um arquivo teste.txt junto a esse arquivo com qualquer coisa dentro 
     fs.readFile('teste.txt', function(err, data) {
-        if(err){
+        if(err) {
             console.log('erro na leitura')
             return response.status(500).send('Erro ao ler o arquivo.')
         }
